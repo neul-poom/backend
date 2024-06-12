91079e560b98b1ceec10b3019d7392f0067b6011
cod2048 <cod2048@gmail.com>
2024-06-12 20:46:11 +0900

    init: 결제 모듈 생성

---
+++ module_payment/src/main/java/com/jk/module_payment/ModulePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.jk.module_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulePaymentApplication.class, args);
+    }
+
+}
---
+++ module_payment/src/test/java/com/jk/module_payment/ModulePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.module_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
