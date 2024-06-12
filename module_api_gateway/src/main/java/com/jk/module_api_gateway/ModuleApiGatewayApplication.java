5e110337cc8fc81e57845be5f19a85e80da5dbe6
cod2048 <cod2048@gmail.com>
2024-06-12 20:47:12 +0900

    init: api gateway 모듈 생성

---
+++ module_api_gateway/src/main/java/com/jk/module_api_gateway/ModuleApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jk.module_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiGatewayApplication.class, args);
+    }
+
+}
