90d183fa55a7c8d34dd3c2edd57342e0b8ba3087
cod2048 <cod2048@gmail.com>
2024-06-12 20:45:05 +0900

    init: 강의 모듈 생성

---
+++ module_lecture/src/main/java/com/jk/module_lecture/ModuleLectureApplication.java
@@ -0,0 +1,13 @@
+package com.jk.module_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleLectureApplication.class, args);
+    }
+
+}
