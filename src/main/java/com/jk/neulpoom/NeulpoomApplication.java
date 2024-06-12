cef6530deba4138c65a77e75cfff3677623961ea
cod2048 <cod2048@gmail.com>
2024-06-12 20:43:41 +0900

    init: first commit

---
+++ src/main/java/com/jk/neulpoom/NeulpoomApplication.java
@@ -0,0 +1,13 @@
+package com.jk.neulpoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeulpoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeulpoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jk/neulpoom/NeulpoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.neulpoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeulpoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
