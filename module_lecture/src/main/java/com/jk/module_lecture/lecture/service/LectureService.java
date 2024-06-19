96fa283e31d24568376ffaf324dd604149e5b52b
cod2048 <cod2048@gmail.com>
2024-06-19 17:08:12 +0900

    feat #3: 강의 CRU 함수 작성 - 강의 수정 시 강의 소유자 확인 - 강의 상세 조회 - 전체 강의 조회

--- module_lecture/src/main/java/com/jk/module_lecture/lecture/service/LectureService.java
+++ module_lecture/src/main/java/com/jk/module_lecture/lecture/service/LectureService.java
@@ -1,13 +1,19 @@
 package com.jk.module_lecture.lecture.service;
 
+import com.jk.module_lecture.common.exception.CustomException;
+import com.jk.module_lecture.common.exception.ErrorCode;
 import com.jk.module_lecture.lecture.entity.Lecture;
 import com.jk.module_lecture.lecture.repository.LectureRepository;
+import com.jk.module_lecture.lecture.service.dto.response.LectureListResponseDto;
+import com.jk.module_lecture.lecture.service.dto.response.LectureDetailResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -36,14 +42,43 @@ public class LectureService {
      * 강의 수정
      */
     @Transactional
-    public Long update(Long lectureId, String title, String description, Long teacherId, BigDecimal price) {
+    public void update(Long lectureId, String title, String description, Long teacherId, BigDecimal price) {
         Lecture lecture = lectureRepository.findById(lectureId)
-                .orElseThrow(() -> new CustomEx)
+                .orElseThrow(() -> new CustomException(ErrorCode.LECTURE_NOT_FOUND));
 
-        Lecture savedLecture = lectureRepository.save(lecture);
+        checkLectureOwner(teacherId, lecture);
 
-        return savedLecture.getLectureId();
+        lecture.update(title, description, price);
     }
 
+    /**
+     * 강의 소유자 확인
+     */
+    private void checkLectureOwner(Long teacherId, Lecture lecture) {
+        if (!teacherId.equals(lecture.getTeacherId())) {
+            throw new CustomException(ErrorCode.INVALID_REQUEST);
+        }
+    }
+
+    /**
+     * 강의 상세 조회
+     */
+    public LectureDetailResponseDto getLectureInfoById(Long lectureId) {
+        Lecture lecture = lectureRepository.findById(lectureId)
+                .orElseThrow(() -> new CustomException(ErrorCode.LECTURE_NOT_FOUND));
+
+        return LectureDetailResponseDto.toDto(lecture);
+    }
+
+    /**
+     * 전체 강의 조회
+     */
+    public List<LectureListResponseDto> getAllLectures() {
+        List<Lecture> lectures = lectureRepository.findAll();
+
+        return lectures.stream()
+                .map(LectureListResponseDto::toDto)
+                .collect(Collectors.toList());
+    }
 }
 
