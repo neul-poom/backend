4a8f0244cbd20764e5692442b4313c1a362a2a9d
cod2048 <cod2048@gmail.com>
2024-06-23 12:11:43 +0900

    feat #7(review): 리뷰 레포 파일 작성

--- module_lecture/src/main/java/com/jk/module_lecture/review/repository/ReviewRepository.java
+++ module_lecture/src/main/java/com/jk/module_lecture/review/repository/ReviewRepository.java
@@ -1,4 +1,17 @@
 package com.jk.module_lecture.review.repository;
 
-public interface ReviewRepository {
+import com.jk.module_lecture.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByLecture_LectureId(Long lectureId);
+
+    List<Review> findByUserId(Long userId);
+
+    List<Review> findByLecture_LectureIdAndUserId(Long lectureId, Long userId);
 }
