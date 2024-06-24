03b2564d2834e89ca19429df90f3a22f4c6a4786
kyungjinleelee <lv_jin@naver.com>
2024-06-24 17:30:34 +0900

    init #4: 기본 쿠폰 테이블 생성 sql문 작성

--- mysql-init/coupon/init.sql
+++ mysql-init/coupon/init.sql
@@ -2,3 +2,14 @@
 CREATE USER 'neulpoom'@'%' IDENTIFIED BY 'neulpoom';
 GRANT ALL PRIVILEGES ON *.* TO 'neulpoom'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS coupon (
+    coupon_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    coupon_code VARBINARY(255) NOT NULL,
+    discount_rate INT NOT NULL,
+    max_quantity BIGINT,
+    issued_quantity BIGINT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    expires_at DATETIME DEFAULT NULL
+);
