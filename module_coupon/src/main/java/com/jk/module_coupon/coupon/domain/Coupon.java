35b2e068ee9ba2f43e0a0c3ac55c88388efa1439
kyungjinleelee <lv_jin@naver.com>
2024-06-24 17:35:52 +0900

    init #4: 쿠폰 엔티티 파일 작성

--- module_coupon/src/main/java/com/jk/module_coupon/coupon/domain/Coupon.java
+++ module_coupon/src/main/java/com/jk/module_coupon/coupon/domain/Coupon.java
@@ -1,9 +1,13 @@
 package com.jk.module_coupon.coupon.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "coupon")
@@ -15,6 +19,38 @@ public class Coupon {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "coupon_id", updatable = false)
     private Long couponId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "coupon_code", nullable = false)
+    private String couponCode;
+
+    @Column(name = "discount_rate", nullable = false)
+    private int discountRate;
+
+    @Column(name = "max_quantity")
+    private Long maxQuantity;
+
+    @Column(name = "issued_quantity")
+    private Long issuedQuantity;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    @Builder
+    public Coupon(String name, String couponCode, int discountRate, Long maxQuantity, Long issuedQuantity, LocalDateTime expiresAt) {
+        this.name = name;
+        this.couponCode = couponCode;
+        this.discountRate = discountRate;
+        this.maxQuantity = maxQuantity;
+        this.issuedQuantity = issuedQuantity;
+        this.expiresAt = expiresAt;
+    }
 
 
 }
