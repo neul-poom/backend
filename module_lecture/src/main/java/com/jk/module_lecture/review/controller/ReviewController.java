3128713e2f2faa202228aa281f676fe73d7237ea
cod2048 <cod2048@gmail.com>
2024-06-24 17:49:25 +0900

    feat #7(review): 리뷰 관련 CRUD 기능 작성 - 리뷰 생성 - 리뷰 수정 - 리뷰 삭제 - 특정 강의 대한 모든 리뷰 조회 - 특정 사용자가 작성한 모든 리뷰 조회

--- module_lecture/src/main/java/com/jk/module_lecture/review/controller/ReviewController.java
+++ module_lecture/src/main/java/com/jk/module_lecture/review/controller/ReviewController.java
@@ -1,4 +1,63 @@
 package com.jk.module_lecture.review.controller;
 
+import com.jk.module_lecture.review.controller.dto.request.ReviewCreateRequestDto;
+import com.jk.module_lecture.review.controller.dto.request.ReviewUpdateRequestDto;
+import com.jk.module_lecture.review.service.ReviewService;
+import com.jk.module_lecture.review.service.dto.ReviewListDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/lectures/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Long> createReview(@RequestBody ReviewCreateRequestDto reviewCreateRequestDto) {
+        Long reviewId = reviewService.createReview(
+                reviewCreateRequestDto.lectureId(),
+                reviewCreateRequestDto.userId(),
+                reviewCreateRequestDto.content(),
+                reviewCreateRequestDto.star()
+        );
+        return new ResponseEntity<>(reviewId, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<Void> updateReview(
+            @PathVariable Long reviewId,
+            @RequestBody ReviewUpdateRequestDto reviewUpdateRequestDto) {
+        reviewService.updateReview(
+                reviewId,
+                reviewUpdateRequestDto.userId(),
+                reviewUpdateRequestDto.content(),
+                reviewUpdateRequestDto.star()
+        );
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Void> deleteReview(
+            @PathVariable Long reviewId) {
+        reviewService.deleteReview(reviewId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/lecture/{lectureId}")
+    public ResponseEntity<List<ReviewListDto>> getReviewsByLectureId(@PathVariable Long lectureId) {
+        List<ReviewListDto> reviews = reviewService.getReviewsByLectureId(lectureId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<ReviewListDto>> getReviewsByUserId(@PathVariable Long userId) {
+        List<ReviewListDto> reviews = reviewService.getReviewsByUserId(userId);
+        return ResponseEntity.ok(reviews);
+    }
 }
