e7b55d3120dc8512434de1e48ebff9bf8af8ed6f
kyungjinleelee <lv_jin@naver.com>
2024-06-24 14:48:16 +0900

    init #4: 기본 클래스 파일 생성

---
+++ module_coupon/src/main/java/com/jk/module_coupon/coupon/controller/CouponController.java
@@ -0,0 +1,11 @@
+package com.jk.module_coupon.coupon.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/coupons")
+public class CouponController {
+}
