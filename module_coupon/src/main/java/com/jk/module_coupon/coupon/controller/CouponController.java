2365d51da5559e29b438d7bb2343b2f1ba4fc545
kyungjinleelee <lv_jin@naver.com>
2024-06-24 17:40:44 +0900

    feat #4: 쿠폰 등록 기능 구현

--- module_coupon/src/main/java/com/jk/module_coupon/coupon/controller/CouponController.java
+++ module_coupon/src/main/java/com/jk/module_coupon/coupon/controller/CouponController.java
@@ -1,11 +1,30 @@
 package com.jk.module_coupon.coupon.controller;
 
+import com.jk.module_coupon.coupon.dto.request.CouponCreateRequestDto;
+import com.jk.module_coupon.coupon.service.CouponService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/coupons")
 public class CouponController {
+    private final CouponService couponService;
+
+    /*
+     * 쿠폰 등록
+     */
+    @PostMapping
+    public ResponseEntity<Void> createCoupon(@Valid @RequestBody CouponCreateRequestDto request) {
+        Long couponId = couponService.createCoupon(request);
+
+        return ResponseEntity.created(URI.create("/api/v1/coupons/" + couponId)).build();
+    }
 }
--- module_coupon/src/main/java/com/jk/module_coupon/coupon/service/CouponService.java
+++ module_coupon/src/main/java/com/jk/module_coupon/coupon/service/CouponService.java
@@ -1,11 +1,38 @@
 package com.jk.module_coupon.coupon.service;
 
+import com.jk.module_coupon.coupon.domain.Coupon;
+import com.jk.module_coupon.coupon.dto.request.CouponCreateRequestDto;
+import com.jk.module_coupon.coupon.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CouponService {
+    private final CouponRepository couponRepository;
+
+    /*
+     * 쿠폰 등록
+     */
+    @Transactional
+    public Long createCoupon(CouponCreateRequestDto request) {
+        Coupon coupon = Coupon.builder()
+                .name(request.name())
+                .couponCode(request.couponCode())
+                .discountRate(request.discountRate())
+                .maxQuantity(request.maxQuantity())
+                .issuedQuantity(request.issuedQuantity() != null ? request.issuedQuantity() : 0L)
+                .expiresAt(request.expiresAt())
+                .build();
+
+        Coupon savedCoupon = couponRepository.save(coupon);
+
+        return savedCoupon.getCouponId();
+    }
+
 }
