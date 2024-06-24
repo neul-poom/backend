2b15ab65084bca02a6bdc57aaafc7c7af5f67974
kyungjinleelee <lv_jin@naver.com>
2024-06-25 00:11:52 +0900

    feat #4: 에러 커스텀 구현 - RuntimeException 상속받아 실행 예외에 Errorcode 필드 추가해 커스텀 - RestControllerAdvice 이용해 전역에서 발생하는 예외 핸들링

---
+++ module_coupon/src/main/java/com/jk/module_coupon/common/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.jk.module_coupon.common.exception;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String message;
+}
