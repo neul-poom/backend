1cce1fb3b59c39502a0e59cff7b6c5dec1987a33
cod2048 <cod2048@gmail.com>
2024-06-25 23:47:41 +0900

    refactor #12(unity_response): 파일을 명확하게 구분하기 위해 파일명에 Dto 추가

--- module_lecture/src/main/java/com/jk/module_lecture/common/dto/ApiResponseDto.java
+++ module_lecture/src/main/java/com/jk/module_lecture/common/dto/ApiResponseDto.java
@@ -2,5 +2,5 @@ package com.jk.module_lecture.common.dto;
 
 import org.springframework.http.HttpStatus;
 
-public record ApiResponse<T>(HttpStatus status, String message, T data) {
+public record ApiResponseDto<T>(HttpStatus status, String message, T data) {
 }
