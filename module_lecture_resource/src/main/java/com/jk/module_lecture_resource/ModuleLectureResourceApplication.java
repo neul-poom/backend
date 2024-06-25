c48457314ccca50fcfd9daf5dd4d0f1986810d04
cod2048 <cod2048@gmail.com>
2024-06-26 02:02:08 +0900

    init #18(lecture_resource): 강의 자료 모듈 생성

---
+++ module_lecture_resource/src/main/java/com/jk/module_lecture_resource/ModuleLectureResourceApplication.java
@@ -0,0 +1,13 @@
+package com.jk.module_lecture_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleLectureResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleLectureResourceApplication.class, args);
+	}
+
+}
