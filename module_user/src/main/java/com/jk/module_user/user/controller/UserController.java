0000ebfd2d499ea000d4ab7335fa21ec79713ec9
easyxun <easyxun@gmail.com>
2024-06-26 02:02:57 +0900

    feat #1(join): 유저 컨트롤러 회원 정보 api 추가

--- module_user/src/main/java/com/jk/module_user/user/controller/UserController.java
+++ module_user/src/main/java/com/jk/module_user/user/controller/UserController.java
@@ -1,18 +1,27 @@
 package com.jk.module_user.user.controller;
 
-import com.jk.module_user.user.dto.request.LoginRequestDto;
+import com.jk.module_user.user.dto.response.UserInfoDto;
 import com.jk.module_user.user.dto.request.SignupRequestDto;
 import com.jk.module_user.user.dto.request.UserPasswordRequestDto;
 import com.jk.module_user.user.dto.request.UserUpdateRequestDto;
 import com.jk.module_user.user.entity.User;
 import com.jk.module_user.user.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.jk.module_user.user.entity.UserRoleEnum;
+import com.jk.module_user.user.security.UserDetailsImpl;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api/v1")
 public class UserController {
@@ -23,7 +32,7 @@ public class UserController {
     }
 
     // 회원 가입 페이지 요청
-    @GetMapping("/signup-page")
+    @GetMapping("/signup")
     public String signupPage() {
         return "signup";
     }
@@ -34,12 +43,22 @@ public class UserController {
         return "login";
     }
 
-    // 회원 가입
+    //회원 가입
     // Body: { "username": String, "password": String, "email": String, "status": String, "profileImg": String (optional), "balance": String, "adminToken": String (optional) }
-    @PostMapping("/signup") // 프론트 email에 담긴 값을  signupRequestDto에 담는다
-    public String signup(@ModelAttribute SignupRequestDto signupRequestDto) {
-        userService.signup(signupRequestDto);
-        return "redirect:/api/v1/login";
+    @PostMapping("/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/v1/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/api/v1/login-page";
     }
 
     // 회원 탈퇴
@@ -78,6 +97,15 @@ public class UserController {
         }
     }
 
+    // 회원 관련 정보 받기
+    @GetMapping("/user-info")
+    @ResponseBody
+    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        String username = userDetails.getUser().getUsername();
+        UserRoleEnum role = userDetails.getUser().getRole();
+        boolean isAdmin = (role == UserRoleEnum.ADMIN);
 
+        return new UserInfoDto(username, isAdmin);
+    }
 
 }
