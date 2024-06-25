9ab7fdf28d6332c5b31d1c5ce3b5df673526d85d
cod2048 <cod2048@gmail.com>
2024-06-25 23:59:24 +0900

    refactor #12(unity_response): 댓글 관련 응답 형식 통일

--- module_lecture/src/main/java/com/jk/module_lecture/review/dto/response/ReviewListResponseDto.java
+++ module_lecture/src/main/java/com/jk/module_lecture/review/dto/response/ReviewListResponseDto.java
@@ -1,10 +1,10 @@
-package com.jk.module_lecture.review.service.dto;
+package com.jk.module_lecture.review.dto.response;
 
 import com.jk.module_lecture.review.entity.Review;
 import lombok.Builder;
 
 @Builder
-public record ReviewListDto(
+public record ReviewListResponseDto(
         Long reviewId,
         Long lectureId,
         Long userId,
@@ -12,8 +12,8 @@ public record ReviewListDto(
         Integer star,
         Boolean status) {
 
-    public static ReviewListDto toDto(Review review) {
-        return ReviewListDto.builder()
+    public static ReviewListResponseDto toDto(Review review) {
+        return ReviewListResponseDto.builder()
                 .reviewId(review.getReviewId())
                 .lectureId(review.getLecture().getLectureId())
                 .userId(review.getUserId())
