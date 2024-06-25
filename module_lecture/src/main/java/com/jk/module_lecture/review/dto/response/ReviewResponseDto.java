0bbe085bb283fddc714dd000f13db232bf6b0c74
cod2048 <cod2048@gmail.com>
2024-06-26 01:03:01 +0900

    feat #7(lecture): 리뷰 조회 시 페이징 처리

--- module_lecture/src/main/java/com/jk/module_lecture/review/dto/response/ReviewResponseDto.java
+++ module_lecture/src/main/java/com/jk/module_lecture/review/dto/response/ReviewResponseDto.java
@@ -1,5 +1,6 @@
 package com.jk.module_lecture.review.dto.response;
 
+import com.jk.module_lecture.review.entity.Review;
 import lombok.Builder;
 
 @Builder
@@ -10,4 +11,15 @@ public record ReviewResponseDto(
         String content,
         Integer star,
         Boolean status) {
+
+    public static ReviewResponseDto toDto(Review review) {
+        return ReviewResponseDto.builder()
+                .reviewId(review.getReviewId())
+                .lectureId(review.getLecture().getLectureId())
+                .userId(review.getUserId())
+                .content(review.getContent())
+                .star(review.getStar())
+                .status(review.getStatus())
+                .build();
+    }
 }
