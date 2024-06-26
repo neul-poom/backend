71eb7b0243ae4e5b599a3a10afd4afacfc9f06f0
easyxun <easyxun@gmail.com>
2024-06-27 06:36:08 +0900

    feat #1(join): Dto 클래스 이름 변경

--- module_user/src/main/java/com/jk/module_user/user/dto/request/UserLoginRequestDto.java
+++ module_user/src/main/java/com/jk/module_user/user/dto/request/UserLoginRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Setter
 @Getter
-public class LoginRequestDto {
+public class UserLoginRequestDto {
     private String username;
     private String password;
 }
--- module_user/src/main/java/com/jk/module_user/user/dto/request/UserSignupRequestDto.java
+++ module_user/src/main/java/com/jk/module_user/user/dto/request/UserSignupRequestDto.java
@@ -1,8 +1,6 @@
 package com.jk.module_user.user.dto.request;
 
-import com.jk.module_user.user.entity.User;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,17 +10,17 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class SignupRequestDto {
+public class UserSignupRequestDto {
 
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
-    @Pattern(regexp = "[a-z0-9].{4,10}", message = "아이디는 4~10자 알파벳 소문자와 숫자로, 4~10자 이내로 구성되어야 합니다.")
+//    @Pattern(regexp = "[a-z0-9].{4,10}", message = "아이디는 4~10자 알파벳 소문자와 숫자로, 4~10자 이내로 구성되어야 합니다.")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$^!%*#?&])[A-Za-z\\d$@$^!%*#?&]{8,15}$", message = "비밀번호는 알파벳 대소문자, 숫자, 특수문자를 포함하여 8~15자여야 합니다.")
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$^!%*#?&])[A-Za-z\\d$@$^!%*#?&]{8,15}$", message = "비밀번호는 알파벳 대소문자, 숫자, 특수문자를 포함하여 8~15자여야 합니다.")
     private String password;
 
-    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+//    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
 
