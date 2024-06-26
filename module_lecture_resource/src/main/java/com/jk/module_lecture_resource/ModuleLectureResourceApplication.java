7e5bd0196b7d22e1ccab479d8f63855b9adaa42a
cod2048 <cod2048@gmail.com>
2024-06-27 01:43:28 +0900

    feat #18(lecture_resource): jpaAuditing 어노테이션 추가

--- module_lecture_resource/src/main/java/com/jk/module_lecture_resource/ModuleLectureResourceApplication.java
+++ module_lecture_resource/src/main/java/com/jk/module_lecture_resource/ModuleLectureResourceApplication.java
@@ -2,8 +2,10 @@ package com.jk.module_lecture_resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ModuleLectureResourceApplication {
 
 	public static void main(String[] args) {
