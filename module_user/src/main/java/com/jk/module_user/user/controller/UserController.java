0073edc45252296396367eacf9c971bac4aeab5b
easyxun <easyxun@gmail.com>
2024-06-27 13:55:28 +0900

    feat #12(unity_response): 로그인, 회원가입 관련 응답 형식 통일

--- module_user/src/main/java/com/jk/module_user/user/controller/UserController.java
+++ module_user/src/main/java/com/jk/module_user/user/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.jk.module_user.user.controller;
 
-import com.jk.module_user.user.dto.response.UserInfoDto;
-import com.jk.module_user.user.dto.request.SignupRequestDto;
+import com.jk.module_user.common.dto.ApiResponseDto;
+import com.jk.module_user.user.dto.response.UserInfoResponseDto;
+import com.jk.module_user.user.dto.request.UserSignupRequestDto;
 import com.jk.module_user.user.dto.request.UserPasswordRequestDto;
 import com.jk.module_user.user.dto.request.UserUpdateRequestDto;
+import com.jk.module_user.user.dto.response.UserSignupResponseDto;
 import com.jk.module_user.user.entity.User;
 import com.jk.module_user.user.service.UserService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +20,12 @@ import com.jk.module_user.user.security.UserDetailsImpl;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
 @Controller
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
 
@@ -46,19 +48,20 @@ public class UserController {
     //회원 가입
     // Body: { "username": String, "password": String, "email": String, "status": String, "profileImg": String (optional), "balance": String, "adminToken": String (optional) }
     @PostMapping("/signup")
-    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+    public ResponseEntity<ApiResponseDto<UserSignupResponseDto>> signup(@Valid @RequestBody UserSignupRequestDto requestDto, BindingResult bindingResult) {
         // Validation 예외처리
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         if (fieldErrors.size() > 0) {
             for (FieldError fieldError : bindingResult.getFieldErrors()) {
                 log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
             }
-            return "redirect:/api/v1/signup";
         }
 
-        userService.signup(requestDto);
+        UserSignupResponseDto userSignupResponseDto = userService.signup(requestDto);
 
-        return "redirect:/api/v1/login-page";
+        return ResponseEntity.created(URI.create("api/v1/users/signup"))
+                .body(new ApiResponseDto<>(HttpStatus.CREATED, "회원가입성공", userSignupResponseDto));
+
     }
 
     // 회원 탈퇴
@@ -88,24 +91,34 @@ public class UserController {
     // 비밀번호 수정
     // Body: { "currentPassword": String, "newPassword": String }
     @PutMapping("/password")
-    public ResponseEntity<String> updatePassword(@RequestBody UserPasswordRequestDto updatePasswordRequestDto, @AuthenticationPrincipal User user) {
+    public ResponseEntity<ApiResponseDto<Void>> updatePassword(@RequestBody UserPasswordRequestDto updatePasswordRequestDto,
+                                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
         try {
+            User user = userDetails.getUser(); // UserDetailsImpl에서 User 객체 가져오기
             userService.updatePassword(user, updatePasswordRequestDto);
-            return ResponseEntity.ok("비밀번호가 수정되었습니다.");
+            return ResponseEntity.ok(new ApiResponseDto<>(HttpStatus.OK, "비밀번호가 수정되었습니다.", null));
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponseDto<>(HttpStatus.BAD_REQUEST, e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), null));
         }
     }
 
     // 회원 관련 정보 받기
     @GetMapping("/user-info")
-    @ResponseBody
-    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        String username = userDetails.getUser().getUsername();
-        UserRoleEnum role = userDetails.getUser().getRole();
-        boolean isAdmin = (role == UserRoleEnum.ADMIN);
+    public ResponseEntity<ApiResponseDto<UserInfoResponseDto>> getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        try {
+            String username = userDetails.getUser().getUsername();
+            UserRoleEnum role = userDetails.getUser().getRole();
+            boolean isAdmin = (role == UserRoleEnum.ADMIN);
 
-        return new UserInfoDto(username, isAdmin);
+            UserInfoResponseDto userInfoResponse = new UserInfoResponseDto(username, isAdmin);
+            return ResponseEntity.ok(new ApiResponseDto<>(HttpStatus.OK, "회원정보조회성공", userInfoResponse));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), null));
+        }
     }
 
 }
