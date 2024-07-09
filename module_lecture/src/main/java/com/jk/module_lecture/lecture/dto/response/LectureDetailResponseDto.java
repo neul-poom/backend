545376a23e0e84cce09fef59ea17fe8cd78f7550
cod2048 <cod2048@gmail.com>
2024-07-09 22:45:28 +0900

    refactor #23: 강의 조회 시 강의 노트, 비디오 함께 조회

--- module_lecture/src/main/java/com/jk/module_lecture/lecture/dto/response/LectureDetailResponseDto.java
+++ module_lecture/src/main/java/com/jk/module_lecture/lecture/dto/response/LectureDetailResponseDto.java
@@ -2,6 +2,7 @@ package com.jk.module_lecture.lecture.dto.response;
 
 import com.jk.module_lecture.lecture.entity.Lecture;
 import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,15 +14,19 @@ public record LectureDetailResponseDto(
         String description,
         Long teacherId,
         BigDecimal price,
+        String fileUrl,
+        String videoUrl,
         LocalDateTime createdAt
 ) {
-    public static LectureDetailResponseDto toDto(Lecture lecture) {
+    public static LectureDetailResponseDto toDto(Lecture lecture, String fileUrl, String videoUrl) {
         return LectureDetailResponseDto.builder()
                 .lectureId(lecture.getLectureId())
                 .title(lecture.getTitle())
                 .description(lecture.getDescription())
                 .teacherId(lecture.getTeacherId())
                 .price(lecture.getPrice())
+                .fileUrl(fileUrl)
+                .videoUrl(videoUrl)
                 .createdAt(lecture.getCreatedAt())
                 .build();
     }
