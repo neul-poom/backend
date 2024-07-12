efce22fe3d5ba818c61a4ff89e81ab5eb8b3c1ff
cod2048 <cod2048@gmail.com>
2024-07-12 16:47:32 +0900

    refactor #42: 페인, jpa 어노테이션 추가

--- module_payment/src/main/java/com/jk/module_payment/ModulePaymentApplication.java
+++ module_payment/src/main/java/com/jk/module_payment/ModulePaymentApplication.java
@@ -2,8 +2,12 @@ package com.jk.module_payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
 public class ModulePaymentApplication {
 
     public static void main(String[] args) {
