f77c1a0c057e15e84616e2aade6ca588f0bf029d
cod2048 <cod2048@gmail.com>
2024-07-12 16:51:49 +0900

    feat #42: 결제 모듈 main 함수 어노테이션 작성

---
+++ module_order/src/main/java/com/jk/module_order/ModuleOrderApplication.java
@@ -0,0 +1,17 @@
+package com.jk.module_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableJpaAuditing
+public class ModuleOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleOrderApplication.class, args);
+    }
+
+}
